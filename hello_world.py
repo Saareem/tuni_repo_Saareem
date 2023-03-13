8ae84ffba7946a3ae69887e8a7c355d8ab2b1e80
Eemeli Pettersson <eemeli.o.pettersson@gmail.com>
2023-03-13 14:59:43 +0200

    Another fix

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 print("Hello world!")
-=======
-<<<<<<< HEAD
-print("Hello World!")
-=======
-print("Hello world!")
->>>>>>> 9bb7a505374c63502be00fe0f9bbfff438b6a484
->>>>>>> 0dc4252ed5e332f45cfb19f1f7037e61bd92213a
